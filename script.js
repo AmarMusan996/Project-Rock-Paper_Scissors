654e42b04bb4d1743d25f118e5360c3e4abfbc7c
AmarMusan996 <amar.musan96@protonmail.com>
2026-06-10 01:00:06 +0200

    The entire code has been redone. The new one is much clener and more readable.

--- script.js
+++ script.js
@@ -1,91 +1,70 @@
-
-// Computer choice
-
 function getComputerChoice() {
-// Gets a random number between 1 and 3
-    let choice = Math.floor(Math.random() * 3) + 1;
-    return choice;
+  const choices = ["rock", "paper", "scissors"]
+  const randomIndex = Math.floor(Math.random() * choices.length)
+  return choices[randomIndex];
 }
 
-// The value of "choice" is the random number from the function getComputerChoice()
-let choice = getComputerChoice();
-
-// Checking and converting the random number in to one of three possible results (Rock, Paper, Scissors)
-    if (choice == 1) {
-            console.log('Rock');
-        }
-
-        else if (choice == 2) {
-            console.log('Paper')
-        }
-
-        else {
-            console.log('Scissors')
-        }
-
-// Capturing the human choice by using the prompt for human to write out the choice
 function getHumanChoice() {
-    let human = window.prompt('Rock, Paper or Scissors?');
-    return human;
+  const choices = prompt('Rock, Paper, or Scissors?', 'Rock').toLowerCase(); // Sta ako osoba klikne "Cancel"? Prompt vrijednost postaje null(null.toLowerCase). Dovest ce do error-a.
+  return choices;
 }
-// Value of "human" is whatever was writen in the prompt in function getHumanChoice()
-let human = getHumanChoice();
-
-// Checking what the human player has written
-if (human == 'Rock') {
-        console.log('Rock');
-        }
-        else if (human == 'Paper') {
-        console.log('Paper');
-        }
-        else { 
-        console.log('Scissors');
-    }
-
-// Score keeper
 
 let humanScore = 0;
 let computerScore = 0;
 
 
-// Single round
-
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase()
-// Covjek dobija rundu
-    if (humanChoice == 'rock' && computerChoice == 'scissors') {
-       humanScore += 1
-    console.log('Rundu dobija igrac');
-    }
-    else if (humanChoice == 'paper' && computerChoice == 'rock') {
-        humanScore += 1
-    console.log('Rundu dobija igrac');
-    }
-    else if (humanChoice == 'scissors' && computerChoice == 'paper') {
-        humanScore += 1
-    console.log('Rundu dobija igrac');
-    }
-// Kompjuter dobija rundu
-    else if (computerChoice == 'rock' && humanChoice == 'scissors') {
-        computerScore += 1
-        console.log('Rundu dobija kompjuter');
-    }
-    else if (computerChoice == 'paper' && humanChoice == 'rock') {
-        computerScore += 1
-        console.log('Rundu dobija kompjuter');
-    }
-    else if (computerChoice == 'scissors' && humanChoice == 'paper') {
-        computerScore += 1
-        console.log('Rundu dobija kompjuter')
-    }
-// Nerijesena runda
-    else if (humanChoice == 'rock' && computerChoice == 'rock') {
-    console.log('Nerijeseno');
-    }
-    else if (humanChoice == 'paper' && computerChoice == 'paper') {
-    console.log('Nerijeseno');
-    }
-    else if (humanChoice == 'scissors' && computerChoice == 'scissors') {
-    console.log('Nerijeseno');
-    }
+// Human winning.  
+  if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    humanScore += 1
+    console.log(`You win! Nice job! The score is ${humanScore} to ${computerScore}`);
+  }
+  else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    humanScore += 1
+    console.log(`You win! Nice job! The score is ${humanScore} to ${computerScore}`);
+  }
+  else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    humanScore += 1
+    console.log(`You win! Nice job! The score is ${humanScore} to ${computerScore}`);
+  }
+// Computer winning.
+  else if (computerChoice === 'rock' && humanChoice === 'scissors') {
+    computerScore += 1
+    console.log(`Unlucky. You've lost. The score is ${humanScore} to ${computerScore}`);
+  }
+  else if (computerChoice === 'scissors' && humanChoice === 'paper') {
+    computerScore += 1
+    console.log(`Unlucky. You've lost. The score is ${humanScore} to ${computerScore}`);
+  }
+  else if (computerChoice === 'paper' && humanChoice === 'rock') {
+    computerScore += 1
+    console.log(`Unlucky. You've lost. The score is ${humanScore} to ${computerScore}`);
+  } 
+// Draw
+  else if (computerChoice === humanChoice) {
+    console.log(`It's a draw. The score remains ${humanScore} to ${computerScore}`);
+  }
+  else {
+    console.log('Wow, not sure what that was, try something else.')
+  }
 }
+
+for (let i = 0; i < 5; i++) {
+  const humanChoice = getHumanChoice()
+  const computerChoice = getComputerChoice()
+  
+  playRound(humanChoice, computerChoice) 
+}
+
+function finalScore(humanScore, computerScore) {
+  if (humanScore === computerScore) {
+    console.log(`Wow, it's a draw. The final score is ${humanScore} for you and ${computerScore} for the machine.`)
+  }
+  else if (humanScore > computerScore) {
+    console.log(`Congradulations, you won! The final score is ${humanScore} for you and ${computerScore} for the machine.`)
+  }
+  else if (humanScore < computerScore) {
+    console.log(`Uhh, you lost. The final score is ${humanScore} for you and ${computerScore} for the machine.`)
+  }
+
+} finalScore(humanScore, computerScore)
